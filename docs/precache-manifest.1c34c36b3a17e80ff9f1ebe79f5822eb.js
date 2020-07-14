610689b1b4119334f6c1456d575d9b79fc864750
Guillermo Puentes <gapuentes@hotmail.com.ar>
2020-07-14 06:46:15 -0300

    Carpta build renombrada a docs

---
+++ docs/precache-manifest.1c34c36b3a17e80ff9f1ebe79f5822eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e3db70245270943d809912f03b1732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b8ac63a01c8da8d0a94",
+    "url": "/static/css/main.72fbad03.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8ac63a01c8da8d0a94",
+    "url": "/static/js/main.061fd965.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
